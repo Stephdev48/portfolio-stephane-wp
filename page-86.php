a712a8e43df6480806ad1dfde04b12b04be09cb9
Stephdev48 <147699498+Stephdev48@users.noreply.github.com>
2023-11-30 14:03:45 +0100

    14h00 màj mineure

--- page-86.php
+++ page-86.php
@@ -1,13 +1,12 @@
-<!-- Contact -->
+                            <!-- Contact -->
 <div class="contact-bg">
-<?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="form">
-    <?php the_content(); ?>
-</div>
-
-<?php get_footer(); ?>
-<?php wp_link_pages(); ?>
+    <?php get_header(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="form">
+            <?php the_content(); ?>
+        </div>
+    <?php get_footer(); ?>
+    <?php wp_link_pages(); ?>
     <?php endwhile; ?>
     <?php endif; ?>
 </div>
--- page-89.php
+++ page-89.php
@@ -1,25 +1,25 @@
-<!-- About -->
+                            <!-- About -->
 <div class="about-section">
     <?php get_header(); ?>
     <section class="section">                       <!--paragraphe de présentation-->
-    <div class="paragraphe">
-        <h1 class="section__title">Hello</h1>
-        <h2 class="section__title__bis">I'm Stéphane from France</h2>
-        <?php $post = get_post(138) ?>
-        <?php echo $post->post_content ?>
-    </div>
+        <div class="paragraphe">
+            <h1 class="section__title">Hello</h1>
+            <h2 class="section__title__bis">I'm Stéphane from France</h2>
+            <?php $post = get_post(138) ?>
+            <?php echo $post->post_content ?>
+        </div>
     </section>
     <section class="pictures">                      <!--photos-->
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-lg-6 animate__animated animate__fadeInLeft animate__delay-0.5s">
-                <a href="http://localhost/wordpress/wp-content/uploads/2023/11/pince-linge.jpg" target="_blank"><img src="http://localhost/wordpress/wp-content/uploads/2023/11/pince-linge.jpg" class="pictures__pince" title="Parc pince à linge" alt="Parc pince à linge"></a>
-            </div>
-            <div class="col-12 col-lg-6 animate__animated animate__fadeInRight animate__delay-0.5s">
-                <a href="http://localhost/wordpress/wp-content/uploads/2023/11/archer.jpg" target="_blank"><img src="http://localhost/wordpress/wp-content/uploads/2023/11/archer.jpg" class="pictures__archer" title="Archer fantasy" alt="Archer fantasy"></a>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 animate__animated animate__fadeInLeft animate__delay-0.5s">
+                    <a href="http://localhost/wordpress/wp-content/uploads/2023/11/pince-linge.jpg" target="_blank"><img src="http://localhost/wordpress/wp-content/uploads/2023/11/pince-linge.jpg" class="pictures__pince" title="Parc pince à linge" alt="Parc pince à linge"></a>
+                </div>
+                <div class="col-12 col-lg-6 animate__animated animate__fadeInRight animate__delay-0.5s">
+                    <a href="http://localhost/wordpress/wp-content/uploads/2023/11/archer.jpg" target="_blank"><img src="http://localhost/wordpress/wp-content/uploads/2023/11/archer.jpg" class="pictures__archer" title="Archer fantasy" alt="Archer fantasy"></a>
+                </div>
             </div>
         </div>
-    </div>
     </section>
     <?php get_footer(); ?>
 </div>
